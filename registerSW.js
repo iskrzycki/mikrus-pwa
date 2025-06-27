45932fd8b08fa7624f70914aff5e4650215b0438
Rafal Iskrzycki <rafal.iskrzycki@allegro.com>
2025-06-27 18:05:26 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mikrus/sw.js', { scope: '/mikrus/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mikrus-pwa/sw.js', { scope: '/mikrus-pwa/' })})}
