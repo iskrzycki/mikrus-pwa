2abbfbb76e44326e13f923311e7b7a5e2fbc2657
Rafal Iskrzycki <rafal.iskrzycki@allegro.com>
2025-08-08 00:21:26 +0200

    assets cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      includeAssets: ["src/assets/icon-72.png", "src/assets/icon-120.png", "src/assets/icon-128.png", "src/assets/icon-144.png", "src/assets/icon-180.png", "src/assets/icon-192.png", "src/assets/icon-512.png", "src/assets/icon-1024.png"],
+      includeAssets: ["public/assets/icon-72.png", "public/assets/icon-120.png", "public/assets/icon-128.png", "public/assets/icon-144.png", "public/assets/icon-180.png", "public/assets/icon-192.png", "public/assets/icon-512.png", "public/assets/icon-1024.png"],
       manifest: {
         name: "mikr.us PWA",
         short_name: "mikr.us",
@@ -16,52 +16,52 @@ export default defineConfig({
         theme_color: "#402B43",
         icons: [
           {
-            "src": "src/assets/icon-72.png",
+            "src": "public/assets/icon-72.png",
             "sizes": "72x72",
             "type": "image/png"
           },
           {
-            "src": "src/assets/icon-120.png",
+            "src": "public/assets/icon-120.png",
             "sizes": "120x120",
             "type": "image/png"
           },
           {
-            "src": "src/assets/icon-128.png",
+            "src": "public/assets/icon-128.png",
             "sizes": "128x128",
             "type": "image/png"
           },
           {
-            "src": "src/assets/icon-144.png",
+            "src": "public/assets/icon-144.png",
             "sizes": "144x144",
             "type": "image/png"
           },
            {
-            "src": "src/assets/icon-152.png",
+            "src": "public/assets/icon-152.png",
             "sizes": "152x152",
             "type": "image/png"
           },
            {
-            "src": "src/assets/icon-167.png",
+            "src": "public/assets/icon-167.png",
             "sizes": "167x167",
             "type": "image/png"
           },
           {
-            "src": "src/assets/icon-180.png",
+            "src": "public/assets/icon-180.png",
             "sizes": "180x180",
             "type": "image/png"
           },
           {
-            "src": "src/assets/icon-192.png",
+            "src": "public/assets/icon-192.png",
             "sizes": "192x192",
             "type": "image/png"
           },
           {
-            "src": "src/assets/icon-512.png",
+            "src": "public/assets/icon-512.png",
             "sizes": "512x512",
             "type": "image/png"
           },
           {
-            "src": "src/assets/icon-1024.png",
+            "src": "public/assets/icon-1024.png",
             "sizes": "1024x1024",
             "type": "image/png"
           },
