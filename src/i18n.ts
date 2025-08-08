ece3f167441d9afc66ff8f3b4b5a0cfba370ce08
Rafal Iskrzycki <rafal.iskrzycki@allegro.com>
2025-08-08 11:10:55 +0200

    i18n fixes

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,10 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-// import LanguageDetector from "i18next-browser-languagedetector";
 
 import enTranslation from "./locales/en.json";
 import plTranslation from "./locales/pl.json";
 
 i18n
-  // .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     debug: process.env.NODE_ENV === "development",
@@ -24,7 +22,6 @@ i18n
     },
     detection: {
       order: ["localStorage"],
-      // order: ["localStorage", "navigator"],
       caches: ["localStorage"],
     },
   });
